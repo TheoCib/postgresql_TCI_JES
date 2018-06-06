0f14d498ef105fc741d8c07d7360041b30a0d066
theophile.cibert@ynov.com <theophile.civert@ynov.com>
2018-06-06 16:36:17 +0200

    end

--- scripts/sql/insert.sql
+++ scripts/sql/insert.sql
@@ -1,19 +1,19 @@
 \pset pager off
 COPY users(user_id,registration_date,firstname,lastname,email,password) FROM
-	'/home/theo/Postgresql/scripts/csv/user.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/user.csv' DELIMITER ',' CSV HEADER;
 
 
 COPY shows(show_id,name,description,release_year,genres,network) FROM
-	'/home/theo/Postgresql/scripts/csv/show.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/show.csv' DELIMITER ',' CSV HEADER;
 
 COPY episodes(episode_id,show_id,season,number,date) FROM
-	'/home/theo/Postgresql/scripts/csv/episode.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/episode.csv' DELIMITER ',' CSV HEADER;
 COPY show_follow(user_id,show_id,follow_date) FROM
-	'/home/theo/Postgresql/scripts/csv/user_follow_show.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/user_follow_show.csv' DELIMITER ',' CSV HEADER;
 COPY show_rating(user_id,show_id,rating,rating_date) FROM
-	'/home/theo/Postgresql/scripts/csv/user_rate_show.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/user_rate_show.csv' DELIMITER ',' CSV HEADER;
 COPY episode_watch(user_id,episode_id,watch_date) FROM
-	'/home/theo/Postgresql/scripts/csv/user_watch_episode.csv' DELIMITER ',' CSV HEADER;
+	'/home/theo/postgresql_TCI_JES/scripts/csv/user_watch_episode.csv' DELIMITER ',' CSV HEADER;
 
 SELECT * FROM users LIMIT 1;
 SELECT * FROM shows LIMIT 1;
